052372360080cb1bb014de4c3c4c49c29dfc8a7e
lichen404 <f360family@gmail.com>
2020-08-19 00:36:56 +0800

    完成基本功能

---
+++ cli.js
@@ -0,0 +1,26 @@
+const {program} = require('commander');
+const api = require('./index.js')
+
+program
+    .option('-a, --add <tasks...>', 'add new task')
+    .option('-c, --clear [task]','clear named task or all tasks')
+
+
+program.parse(process.argv);
+if(program.add){
+   api.add(program.add).then(()=>{
+       console.log('add success')
+   }).catch((e)=>{
+       console.log(e)
+   })
+}
+if(program.clear){
+   api.clear().then(()=>{
+       console.log('clear')
+   }).catch((e)=>{
+       console.log(e)
+   })
+}
+if(process.argv.length===2){
+    void api.showAll()
+}
---
+++ index.js
@@ -0,0 +1,97 @@
+const db = require('./db.js')
+const inquirer = require('inquirer')
+
+module.exports.add = async (titles) => {
+
+    const list = await db.read().catch(e => {
+        console.log(e)
+    })
+    const titleList = titles.map((title) => {
+        return {
+            title,
+            done: false
+        }
+    })
+    list.push(...titleList)
+    await db.write(list).catch(e => {
+        console.log(e)
+    })
+}
+module.exports.clear = async () => {
+    await db.write([])
+}
+module.exports.showAll = async () => {
+    const list = await db.read().catch(e => {
+        console.log(e)
+    })
+    list.forEach((task, index) => {
+        console.log(`${index + 1} - ${task.title}  ${task.done ? '(finished)' : '(unfinished)'}`)
+    })
+    inquirer.prompt(
+        {
+            type: 'list',
+            name: 'index',
+            message: 'please select the task you have finished and press enter.',
+            choices: [...list.map((task, index) => {
+                    return {
+                        name: `${index + 1} - ${task.title}  ${task.done ? '(finished)' : '(unfinished)'}`,
+                        value: index.toString()
+                    }
+
+                }
+            ), {name: 'new task', value: '-2'}, {name: 'exit', value: '-1'}]
+
+        }
+    ).then((answer) => {
+        const index = parseInt(answer.index)
+        if (index >= 0) {
+            inquirer.prompt({
+                type: 'list', name: 'action',
+                message: 'please select your action and press enter.',
+                choices: ['mark as finished', 'mark as unfinished', 'remove', 'update title', 'quit']
+            }).then((answer) => {
+                switch (answer.action) {
+                    case 'mark as finished':
+                        list[index].done = true
+                        db.write(list)
+                        break;
+                    case  'mark as unfinished':
+                        list[index].done = false
+                        db.write(list)
+                        break;
+                    case 'remove':
+                        list.splice(index, 1)
+                        db.write(list)
+                        break;
+                    case 'update title':
+                        inquirer.prompt({
+                            type: 'input',
+                            name: 'title',
+                            message: "new title",
+                            default: list[index].title
+                        }).then(answer => {
+                            list[index].title = answer.title
+                            db.write(list)
+                        })
+                        break;
+
+
+                }
+            })
+        } else if (index === -2) {
+            inquirer.prompt({
+                type: 'input',
+                name: 'title',
+                message: "enter task title.",
+
+            }).then(answer => {
+                list.push({
+                    title: answer.title,
+                    done: false
+                })
+                db.write(list)
+            })
+        }
+
+    })
+}
